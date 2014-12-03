b27c34da3320f09134fcd4cd4ecc5feac29c6d1d
konsi1996 <konstantin.bolshakov.96@gmail.com>
2014-12-03 14:40:06 +0100

    Revert "New Header"
    This reverts commit 7857935e6634fc7a41df9cd17c8064aeb274234f.

---
+++ js/libs/jqueryui/jquery.ui.datepicker-de.js
@@ -0,0 +1,32 @@
+/* German initialisation for the jQuery UI date picker plugin. */
+/* Written by Milian Wolff (mail@milianw.de). */
+(function( factory ) {
+if ( typeof define === "function" && define.amd ) {
+// AMD. Register as an anonymous module.
+define([ "../datepicker" ], factory );
+} else {
+// Browser globals
+factory( jQuery.datepicker );
+}
+}(function( datepicker ) {
+datepicker.regional['de'] = {
+closeText: 'Schlieﬂen',
+prevText: '&#x3C;Zur&uuml;ck',
+nextText: 'Vor&#x3E;',
+currentText: 'Heute',
+monthNames: ['Januar','Februar','M&auml;rz','April','Mai','Juni',
+'Juli','August','September','Oktober','November','Dezember'],
+monthNamesShort: ['Jan','Feb','M&auml;r','Apr','Mai','Jun',
+'Jul','Aug','Sep','Okt','Nov','Dez'],
+dayNames: ['Sonntag','Montag','Dienstag','Mittwoch','Donnerstag','Freitag','Samstag'],
+dayNamesShort: ['So','Mo','Di','Mi','Do','Fr','Sa'],
+dayNamesMin: ['So','Mo','Di','Mi','Do','Fr','Sa'],
+weekHeader: 'KW',
+dateFormat: 'dd.mm.yy',
+firstDay: 1,
+isRTL: false,
+showMonthAfterYear: false,
+yearSuffix: ''};
+datepicker.setDefaults(datepicker.regional['de']);
+return datepicker.regional['de'];
+}));
