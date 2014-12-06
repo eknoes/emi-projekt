dd4f28a2ffcb828b956d7355582933a723831455
konsi1996 <konstantin.bolshakov.96@gmail.com>
2014-12-06 23:23:52 +0100

    Added Sample "Infos" and "Impressum"
    Alex:
    -Design is getting ready (Quickinfo, Preview Images, Details, Maps and
    Directions)
    -Changed background color again to a lighter grey
    Konsi:
    -Impressum with javascript (Bug: double-click)

--- js/header.js
+++ js/header.js
@@ -24,7 +24,8 @@ $(function(){
 function resizenavi (){
     var breite = $(window).width(); /*Calculate window width*/
     if (breite>=1000) {
-        $("a").css("min-width", breite/8.617); /*adjust button size to window*/
+        $("a.navbutton").css("min-width", breite/8.617); /*adjust button size to window*/
+        $("a.impbutton").css("min-width", breite/12);
         $("#navi").css("width", "100%");
     }
     else {
@@ -35,3 +36,37 @@ function resizenavi (){
 $(window).resize(function (){
     resizenavi();
 });
+function details(info){
+    var text, aktiv;
+    switch (info) {
+        case 'team':
+            text = "<b>Team</b>";
+            break;
+        case 'styleguide':
+            text = "styleguide";
+            break;
+        case 'mockups':
+            text = "Mockups";
+            break;
+    }
+    aktiv = document.getElementById("impdetails").innerHTML;
+    document.getElementById("impdetails").innerHTML=text;
+    
+        if ($("#impdetails").css('display') === 'none') {
+            if (aktiv !== text) {
+                $("#impdetails").css("display", "block");
+            }
+            else {
+                $("#impdetails").css("display", "none");
+            }
+   
+        }
+        else {
+            if (aktiv === text) {
+                $("#impdetails").css("display", "none");
+            }
+            else {
+                $("#impdetails").css("display", "block");
+            }
+        }
+}
