caf5ccbf29598589d60587d3c57211e2c462edb2
konsi1996 <konstantin.bolshakov.96@gmail.com>
2014-12-26 20:39:27 +0100

    Fix mobile display and animation added
    HTML:
    -deleted input tag with invisible checkbox needed for nav bar (works now
    with jquery)
    -set anchor tags for navigation buttons
    -put event circles in easy editable div tag
    -maps got an id now
    
    CSS:
    -deleted all styles for said input tag
    -reduced size of search field to avoid overflow with nav bar at low
    resolutions
    
    JS:
    -"inhalt" div and children change styles on resize, in particular on
    mobile devices, now (hopefully) no overflows and good display
    -found great jquery function (toggle) for "impressum" tabs, deleted
    non-efficient code
    -smooth scrolling animations
    -nav bar toggling now works with jquery (earlier: CSS)

--- js/header.js
+++ js/header.js
@@ -26,9 +26,43 @@ function resizenavi (){
     if (breite>=1000) {
         $("a.navbutton").css("min-width", breite/8.617); /*adjust button size to window*/    
         $("#navi").css("width", "100%");
+        $(".navbuttonmobile").switchClass("navbuttonmobile", "navbutton");
+        /*show navigation bar in case it was hidden */
+        $("#Navigation").show();
+        /*Quickinfo Table full sized*/
+        $("#ContentsTable").css("width", "50%");
+        /*Puts preview pictures next to quick info text*/
+        $("#infoPictures").css("position", "absolute");
+        $("#infoBlock1").css("height", "250px");
+        $("#infoPictures").css("width", "39%");
+        $("#titleQuickInfo").css("width", "59%");
+        /*Make content div smaller*/
+        $("#inhalt").css("width", "70%");
+        /*set map size initial*/
+        $("#maps").css("width", "600px"); 
+        $("#infoBlockAnfahrtText").css("position", "absolute");
+        $("#infoBlockAnfahrtText").css("display", "inline-block");
+        $("#infoBlockAnfahrtText").css("top", "50%");
+        $("#infoBlockAnfahrtText").css("transform", "translate(0px, -50%)");
     }
     else {
         $("#Navigation").css("margin-left", "0");
+        $(".navbutton").switchClass("navbutton", "navbuttonmobile");
+        /*Quickinfo Table full sized*/
+        $("#ContentsTable").css("width", "100%");
+        /*Puts preview pictures below quick info text*/
+        $("#infoPictures").css("position", "static");
+        $("#infoBlock1").css("height", "auto");
+        $("#infoPictures").css("width", "100%");
+        $("#titleQuickInfo").css("width", "100%");
+        /*Make content div wider*/
+        $("#inhalt").css("width", "98%");
+         /*resize maps size to fit screen*/
+        $("#maps").css("width", "100%");
+        $("#infoBlockAnfahrtText").css("position", "static");
+        $("#infoBlockAnfahrtText").css("display", "inline");
+        $("#infoBlockAnfahrtText").css("top", "0");
+        $("#infoBlockAnfahrtText").css("transform", "none");
     }
     $("a.impbutton").css("min-width", breite/12);
 }
@@ -36,39 +70,48 @@ function resizenavi (){
 $(window).resize(function (){
     resizenavi();
 });
+/*Impressum tab hide/show*/
 function details(info){
-    var text, aktiv;
-    function display(id) {
-        if ($(id).css('display') === 'none') {
-            $(id).css("display", "block"); 
-        }
-        else {     
-            $(id).css("display", "none");
-        }
-        switch (id) {
-            case '#team':
-                $('#styleguide').css("display", "none");
-                $('#mockups').css("display", "none");
-                break;
-            case '#styleguide':
-                $('#mockups').css("display", "none");
-                $('#team').css("display", "none");
-                break;
-            case '#mockups':
-                $('#styleguide').css("display", "none");
-                $('#team').css("display", "none");
-                break;
-        }
-    }
     switch (info) {
         case 'team':
-            display('#team');
+            $("#team").toggle("fast");
+            $("#styleguide").hide();
+            $("#mockups").hide();
             break;
         case 'styleguide':
-            display('#styleguide');
+            $("#styleguide").toggle("fast");
+            $("#mockups").hide();
+            $("#team").hide();
             break;
         case 'mockups':
-            display('#mockups');
+            $("#mockups").toggle("fast");
+            $("#styleguide").hide();
+            $("#team").hide();
             break;
-    }
+    }    
 }
+/*Smooth scrolling*/
+$(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+	    var target = this.hash;
+	    var $target = $(target);
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+                /*window.location.hash = target; shows anchor in URL*/
+	    });
+            /*hide mobile Navigation onclick*/
+            if($('#Navigation li').find('a.navbuttonmobile').length !== 0){
+                $("#Navigation").hide();
+            }
+            
+	});
+});
+/*toggle mobile navigation*/
+$(function(){
+    $(".show-menu").click(function(){
+        $("#Navigation").toggle("fast");
+    });
+});
+
