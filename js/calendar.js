452890ae3b07f3748c9930e1321a70291ff6cdab
callepokoj <calle14@hotmail.de>
2015-01-06 17:42:42 +0100

    bakba

--- js/calendar.js
+++ js/calendar.js
@@ -27,19 +27,19 @@ function generateMonth(month, year) {
         dt = new Date(year, month, ct);
         ct--;
     }
-	
-	if((dt.getDay() === 0) ||  (dt.getDay() === 5) || (dt.getDay() === 6)){
-		rows = 6;
-	}
-	
+
+    if ((dt.getDay() === 0) || (dt.getDay() === 5) || (dt.getDay() === 6)) {
+        rows = 6;
+    }
+
     //laufe ueber den Monat und speichere die einzelnen Daten in ein Array
     for (var i = 0; i < 42; i++) {
         cal_sheet[i] = dt;
-        
-		if (dt.getDate() === 15) {
+
+        if (dt.getDate() === 15) {
             date = dt;
         }
-		
+
         dt = new Date(dt.getFullYear(), dt.getMonth(), dt.getDate() + 1);
     }
 
@@ -56,39 +56,39 @@ function prevMonth() {
 }
 
 function write() {
+
     var count = 0;
+	
     text = ' <table id="calendar"><tr><td class="calendar_head"><a class="calendar_link" href="javascript:prevMonth()"> &laquo;</a></td><td colspan=5 class="calendar_head_month" id="calendar_month">'
             + MONTH_NAME[date.getMonth()] + ' ' + date.getFullYear()
             + '</td><td class="calendar_head"><a class="calendar_link" href="javascript:nextMonth()"> &raquo;</a></td>'
             + '</tr><tr><td class="calendar_day">Mo</td><td class="calendar_day">Di</td><td class="calendar_day">Mi</td>'
             + '<td class="calendar_day">Do</td><td class="calendar_day">Fr</td><td class="calendar_day">Sa</td><td class="calendar_day">So</td></tr>';
+			
     for (var i = 0; i < 6; i++) {
         text = text + '<tr>';
         for (var j = 0; j < 7; j++) {
-		
-			if(cal_sheet[count].getMonth() !== date.getMonth()){
-				text = text + '<td class="calendar_entry out_of_this_month" id="' + date.getFullYear() + '-'
-				+ cal_sheet[count].getMonth() + 1 + '-' + cal_sheet[count].getDate() + '">' + cal_sheet[count].getDate() +
-                    ' </div></td>';
-			}else{
-			
-				
-				if(cal_sheet[count].getDate() === new Date().getDate() && cal_sheet[count].getMonth() === new Date().getMonth()){
-					text = text + '<td class="calendar_entry current_day" id="' + date.getFullYear() + '-' + cal_sheet[count].getMonth() + 1 
-					+ '-' + cal_sheet[count].getDate() + '">' + cal_sheet[count].getDate() +
-                                ' </div></td>';
-				}else{
-                                    text = text + '<td class="calendar_entry" id="' + date.getFullYear() + '-' + cal_sheet[count].getMonth() + 1 
-                                    + '-' + cal_sheet[count].getDate() + '">' + cal_sheet[count].getDate() +
-                                    ' </div></td>';
-                                }		
-			}
-            
+
+            if (cal_sheet[count].getMonth() !== date.getMonth()) {
+                text = text + '<td class="calendar_entry out_of_this_month" id="' + date.getFullYear() + '-'
+                        + cal_sheet[count].getMonth() + 1 + '-' + cal_sheet[count].getDate() + '">' + cal_sheet[count].getDate() +
+                        '</div></td>';
+            } else {
+                if (cal_sheet[count].getDate() === new Date().getDate() && cal_sheet[count].getMonth() === new Date().getMonth() && cal_sheet[count].getFullYear() === new Date().getFullYear()) {
+                    text = text + '<td class="calendar_entry current_day" id="' + date.getFullYear() + '-' + cal_sheet[count].getMonth() + 1
+                            + '-' + cal_sheet[count].getDate() + '">' + cal_sheet[count].getDate() +
+                            ' </div></td>';
+                } else {
+                    text = text + '<td class="calendar_entry" id="' + date.getFullYear() + '-' + cal_sheet[count].getMonth() + 1
+                            + '-' + cal_sheet[count].getDate() + '">' + cal_sheet[count].getDate() +
+                            '</div></td>';
+                }
+            }
+
             count++;
         }
         text = text + '</tr>';
     }
     text = text + '</table>';
     document.getElementById("kalender").innerHTML = text;
-//    document.write(DAY_NAME[dt.getDay()] + ", der " + dt.getDate() + ". " + MONTH_NAME[dt.getMonth()] + " " + dt.getFullYear() + "<br>");
 }
